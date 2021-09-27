a1a136bcddefbf2c8dcaecf9489278fcdb0049c0
Ahmed Elfakharany <a.elfakharany@activevideo.com>
2021-09-28 01:00:54 +0200

    Everything works

--- funda/funda.go
+++ funda/funda.go
@@ -1,7 +1,140 @@
 package funda
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/gocolly/colly/v2"
+	"log"
+	"strings"
+	"time"
+)
 
-func PrintHelp(){
-	fmt.Println("Hello")
+type Listing struct {
+	Title            string
+	Address          string
+	LivingArea       string
+	Bedrooms         string
+	Price            string
+	ListedSince      string
+	Status           string
+	ConstructionYear string
+	Facilities       string
+	EnergyLabel      string
+	Insulation       string
+	URL              string
+}
+
+var err error
+var currentURL string
+
+func GetListings(domain string, city string) []Listing {
+	c := colly.NewCollector(
+		colly.AllowedDomains(domain, "www."+domain),
+		colly.UserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"),
+		colly.CacheDir("./listings_cache"),
+	)
+	err = c.Limit(&colly.LimitRule{
+		DomainGlob:  "*"+domain+".*",
+		Parallelism: 2,
+		RandomDelay: 5 * time.Second,
+	})
+	if err != nil {
+		fmt.Println("Error while limiting scraping rules:", err)
+	}
+
+	// Create another collector to scrape listing details
+	detailCollector := c.Clone()
+
+	var listings []Listing
+	c.OnHTML("div.search-result__header-title-col", func(e *colly.HTMLElement) {
+		e.DOM.Find("a").EachWithBreak(func(_ int, s *goquery.Selection) bool {
+			link, _ := s.Attr("href")
+			currentURL = "https://www." + domain + link
+			err = detailCollector.Visit(currentURL)
+			if err != nil {
+				fmt.Println(err)
+			}
+			return false
+		})
+	})
+	c.OnHTML("a[rel=next]", func(e *colly.HTMLElement) {
+		link := "https://www." + domain + e.Attr("href")
+		err = c.Visit(link)
+		if err != nil {
+			fmt.Println("Error while trying to visit", link, err)
+		}
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		log.Println("visiting", r.URL.String())
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	detailCollector.OnHTML("div.object-primary", func(e *colly.HTMLElement) {
+		title := strings.TrimSpace(e.DOM.Find("span.object-header__title").Text())
+		fullAddress := strings.TrimSpace(e.DOM.Find("span.object-header__subtitle.fd-color-dark-3").Text())
+		field := strings.Split(fullAddress, "\n")
+		address := strings.TrimSpace(field[0])
+		var city string
+		if len(field) > 1 {
+			city = strings.TrimSpace(field[1])
+		}
+		var livingArea, bedrooms, price, listedSince, status, constructionYear, facilities, energyLabel, insulation string
+		e.DOM.Find("dt").Each(func(i int, s *goquery.Selection) {
+			dd := s.NextFiltered("dd")
+			if s.Text() == "Living area" {
+				livingArea = strings.TrimSpace(dd.Text())
+			}
+			if s.Text() == "Number of rooms" {
+				bedrooms = strings.TrimSpace(dd.Text())
+			}
+			if s.Text() == "Asking price" {
+				price = strings.Replace(strings.TrimSpace(dd.Text()), " kosten koper", "", -1)
+			}
+			if strings.Contains(s.Text(), "Listed since") {
+				listedSince = strings.TrimSpace(dd.Text())
+			}
+			if strings.Contains(s.Text(), "Status") {
+				status = strings.TrimSpace(dd.Text())
+			}
+			if strings.Contains(s.Text(), "Year of construction") {
+				constructionYear = strings.TrimSpace(dd.Text())
+			}
+			if strings.Contains(s.Text(), "Construction period") {
+				constructionYear = strings.TrimSpace(dd.Text())
+			}
+			if strings.Contains(s.Text(), "Facilities") {
+				if facilities != "" {
+					facilities = facilities + ", " + strings.TrimSpace(dd.Text())
+				} else {
+					facilities = strings.TrimSpace(dd.Text())
+				}
+			}
+			if strings.Contains(s.Text(), "Energy label") {
+				energyLabel = strings.Split(strings.TrimSpace(dd.Text()), "\n")[0]
+			}
+			if strings.Contains(s.Text(), "Insulation") {
+				insulation = strings.Split(strings.TrimSpace(dd.Text()), "\n")[0]
+			}
+		})
+		listing := Listing{
+			Title:            title,
+			Address:          address + " " + city,
+			LivingArea:       livingArea,
+			Bedrooms:         bedrooms,
+			Price:            price,
+			ListedSince:      listedSince,
+			Status:           status,
+			ConstructionYear: constructionYear,
+			Facilities:       facilities,
+			EnergyLabel:      energyLabel,
+			Insulation:       insulation,
+			URL:              currentURL,
+		}
+		listings = append(listings, listing)
+	})
+	err = c.Visit("https://www." + domain + "/en/koop/" + city + "/")
+	return listings
 }
--- main.go
+++ main.go
@@ -1,113 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"github.com/PuerkitoBio/goquery"
-	"github.com/gocolly/colly/v2"
+	"encoding/json"
+	"github.com/abohmeed/go-funda/funda"
 	"log"
 	"os"
 )
 
-type Course struct {
-	Title       string
-	Description string
-	Creator     string
-	Level       string
-	URL         string
-	Language    string
-	Commitment  string
-	Rating      string
-}
-
 func main() {
-	fName := "houses.json"
+	city := os.Args[1]
+	fName := city + ".json"
 	file, err := os.Create(fName)
 	if err != nil {
 		log.Fatalf("Cannot create file %q: %s\n", fName, err)
 		return
 	}
 	defer file.Close()
+	listings := funda.GetListings("funda.nl",city)
+	enc := json.NewEncoder(file)
+	enc.SetIndent("", "  ")
 
-	// Instantiate default collector
-	c := colly.NewCollector(
-		// Visit only domains: coursera.org, www.coursera.org
-		colly.AllowedDomains("funda.nl","www.funda.nl"),
-		colly.UserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"),
-
-		// Cache responses to prevent multiple download of pages
-		// even if the collector is restarted
-		colly.CacheDir("./coursera_cache"),
-	)
-
-	// Create another collector to scrape course details
-	//detailCollector := c.Clone()
-
-	//courses := make([]Course, 0, 200)
-	c.OnHTML("div.search-result__header-title-col", func(e *colly.HTMLElement) {
-		e.DOM.Find("a").EachWithBreak(func(_ int, s *goquery.Selection) bool {
-			fmt.Println(s.Attr("href"))
-			return false
-		})
-	})
-
-	// Before making a request print "Visiting ..."
-	c.OnRequest(func(r *colly.Request) {
-		log.Println("visiting", r.URL.String())
-	})
-	err = c.Visit("https://www.funda.nl/en/koop/amersfoort/")
-	if err != nil {
-		fmt.Println(err)
-	}
+	// Dump json to the standard output
+	enc.Encode(listings)
 }
-
-// On every <a> element with collection-product-card class call callback
-//	c.OnHTML(`a.collection-product-card`, func(e *colly.HTMLElement) {
-//		// Activate detailCollector if the link contains "coursera.org/learn"
-//		courseURL := e.Request.AbsoluteURL(e.Attr("href"))
-//		if strings.Index(courseURL, "coursera.org/learn") != -1 {
-//			detailCollector.Visit(courseURL)
-//		}
-//	})
-//
-//	// Extract details of the course
-//	detailCollector.OnHTML(`div[id=rendered-content]`, func(e *colly.HTMLElement) {
-//		log.Println("Course found", e.Request.URL)
-//		title := e.ChildText(".banner-title")
-//		if title == "" {
-//			log.Println("No title found", e.Request.URL)
-//		}
-//		course := Course{
-//			Title:       title,
-//			URL:         e.Request.URL.String(),
-//			Description: e.ChildText("div.content"),
-//			Creator:     e.ChildText("li.banner-instructor-info > a > div > div > span"),
-//			Rating:      e.ChildText("span.number-rating"),
-//		}
-//		// Iterate over div components and add details to course
-//		e.ForEach(".AboutCourse .ProductGlance > div", func(_ int, el *colly.HTMLElement) {
-//			svgTitle := strings.Split(el.ChildText("div:nth-child(1) svg title"), " ")
-//			lastWord := svgTitle[len(svgTitle)-1]
-//			switch lastWord {
-//			// svg Title: Available Langauges
-//			case "languages":
-//				course.Language = el.ChildText("div:nth-child(2) > div:nth-child(1)")
-//			// svg Title: Mixed/Beginner/Intermediate/Advanced Level
-//			case "Level":
-//				course.Level = el.ChildText("div:nth-child(2) > div:nth-child(1)")
-//			// svg Title: Hours to complete
-//			case "complete":
-//				course.Commitment = el.ChildText("div:nth-child(2) > div:nth-child(1)")
-//			}
-//		})
-//		courses = append(courses, course)
-//	})
-//
-//	// Start scraping on http://coursera.com/browse
-//	c.Visit("https://coursera.org/browse")
-//
-//	enc := json.NewEncoder(file)
-//	enc.SetIndent("", "  ")
-//
-//	// Dump json to the standard output
-//	enc.Encode(courses)
-//}
